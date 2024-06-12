df1ab0b86de08d3ad30c1fb81ce5427c4509cfa6
Jack <59969032+jackkieny@users.noreply.github.com>
2024-06-12 15:00:39 -0500

    Add password reveal to home login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,11 +2,19 @@ import React, { useState } from 'react'
 import { Link, useNavigate, useLocation } from 'react-router-dom' 
 import '../styles/login.css'
 
+import { FiEye, FiEyeOff } from "react-icons/fi";
+
 function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [response, setResponse] = useState(null)
+    const [showPassword, setShowPassword] = useState(false)
+
     const navigate = useNavigate()
+
+    const viewPassword = () => {
+        setShowPassword(!showPassword)
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -42,12 +50,18 @@ function Login() {
                 />
 
                 <label className='login-form-label'>Password</label>
-                <input 
-                    type='password' 
-                    className='login-form-input' 
-                    value={password} 
-                    onChange={(e) => setPassword(e.target.value)} 
-                />
+                <div className='login-form-password-container'>
+                    <input 
+                        type={showPassword ? 'text' : 'password'}
+                        className='login-form-input' 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                    <div className="login-form-password-eye">
+                        {showPassword ? <FiEyeOff onClick={viewPassword} /> : <FiEye onClick={viewPassword} />}
+                    </div>
+
+                </div>
 
                 <button className='login-form-button'>Login</button>
             </form>
