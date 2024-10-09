75308c0dcaf8d3a955032efe41d5212bc499318a
Jack Kieny <jackkieny@cox.net>
2024-10-09 00:23:21 -0500

    batman

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "log"
+
+    "github.com/gofiber/fiber/v2"
+)
+
+func main(){
+    log.Println("Starting server...")
+
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+    log.Fatal(app.Listen(":3000"))
+}
+    
