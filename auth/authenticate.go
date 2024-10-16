1e5dab63dda4911ea8acbaf66e76dfcf1e80eec6
Jack Kieny <jackkieny@cox.net>
2024-10-16 17:03:15 -0500

    Add authentication for protected routes

---
+++ auth/authenticate.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/session"
+)
+
+func Authenticate(store *session.Store) fiber.Handler {
+    return func(c *fiber.Ctx) error {
+        sess, err := store.Get(c)
+        if err != nil {
+            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+                "error": "Error when retrieving the store",
+            })
+        }
+
+        userId := sess.Get("userId")
+        if userId == nil {
+            return c.Status(fiber.StatusUnauthorized).SendString("Unauthorized")
+        }
+
+        return c.Next()
+    }
+}
+
