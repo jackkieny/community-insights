1628429d4a896209e0db68ec185f91e94d77991f
Jack Kieny <jackkieny@cox.net>
2024-10-24 12:01:51 -0500

    chore: cleanup

--- handlers/login_to_skool.go
+++ handlers/login_to_skool.go
@@ -12,50 +12,51 @@ import (
 )
 
 type LoginPayload struct {
-    Email    string `json:"email"`
-    Password string `json:"password"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func LoginToSkool(email, password string) (string, error) {
-    if err := godotenv.Load(); err != nil {
-        log.Fatalf("Error loading .env file, %v", err)
-    }
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file, %v", err)
+	}
 
-    url := os.Getenv("SKOOL_LOGIN_URL")
+	url := os.Getenv("SKOOL_LOGIN_URL")
 
-    payload := LoginPayload{
-        Email:    email,
-        Password: password,
-    }
+	payload := LoginPayload{
+		Email:    email,
+		Password: password,
+	}
 
-    jsonPayload, err := json.Marshal(payload)
-    if err != nil { 
-        return "", err
-    }
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
 
-    request, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
-    if err != nil {
-        return "", err
-    }
+	request, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return "", err
+	}
 
-    request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Content-Type", "application/json")
 
-    webClient := &http.Client{}
-    response, err := webClient.Do(request)
-    if err != nil {
-        return "", err
-    }
+	webClient := &http.Client{}
+	response, err := webClient.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
 
-    if response.StatusCode != http.StatusOK {
-        return "", fmt.Errorf("Did not receive 200 status code. Got %d", response.StatusCode)
-    }
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Did not receive 200 status code. Got %d", response.StatusCode)
+	}
 
-    cookies := response.Cookies()
-    for _, cookie := range cookies {
-        if cookie.Name == "auth_token" {
-            return cookie.Value, nil
-        }
-    }
+	cookies := response.Cookies()
+	for _, cookie := range cookies {
+		if cookie.Name == "auth_token" {
+			return cookie.Value, nil
+		}
+	}
 
-    return "", fmt.Errorf("Could not find auth_token in response")
+	return "", fmt.Errorf("Could not find auth_token in response")
 }
