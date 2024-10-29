9490e76f6e675b19caf6051739a4a30669307fd8
Jack Kieny <jackkieny@cox.net>
2024-10-29 15:55:35 -0500

    Add Error Page

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,17 @@
-import './app.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './Home/Home';
+import Error404 from './Error/Error404';
 
 function App() {
   return (
-      <div className="app">
-        <h1>Hello World</h1>
-      </div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="*" element={<Error404/>} />
+          </Routes>
+        </Router>
   );
 }
 
