8afc1443f54498adba701cf61caeab9eda95572c
Jack Kieny <jackkieny@cox.net>
2024-10-29 16:08:06 -0500

    Add button functionality for error page

--- fe/src/Error/Error404.jsx
+++ fe/src/Error/Error404.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './error404.css'
 
 function Error404() {
+  const navigate = useNavigate()
+
+  const HandleClick = () => {
+    navigate('/')
+  }
+
   return (
     <div className='error404-container'>
       <div className="error404-content">
         <p className="error404-number">404</p>
         <h1>Page Not Found</h1>
         <p className="error404-message">
-            Oops, it seems like the page you're looking for isn't available or doesn't exist.
+          Oops, it seems like the page you're looking for isn't available or doesn't exist.
         </p>
         <p className="error404-message">
-            Please check the URL and try again.
+          Please check the URL and try again.
         </p>
-        <button className="error404-button">
-            <Link to="/" className="error404-link">HOME</Link>
+        <button className="error404-button" onClick={HandleClick}>
+          <Link to="/" className="error404-link">HOME</Link>
         </button>
       </div>
     </div>
