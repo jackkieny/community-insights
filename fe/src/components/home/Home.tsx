ddfc47e84b99b1d6b4bc1a422903b5c86adf83af
Jack Kieny <jackkieny@cox.net>
2024-10-31 20:25:14 -0500

    Add stats section

--- fe/src/components/home/Home.tsx
+++ fe/src/components/home/Home.tsx
@@ -1,5 +1,6 @@
 import { Header } from './header/Header'
 import { Hero } from './hero/Hero'
+import { Stats } from './stats/Stats'
 import { Footer } from './footer/Footer'
 
 export function Home() {
@@ -7,6 +8,7 @@ export function Home() {
     <>
       <Header />
       <Hero />
+      <Stats />
       <Footer />
     </>
   )
