34091905772ae5a92aba945e4504dacdc59538c9
Jack Kieny <jackkieny@cox.net>
2025-01-13 16:51:43 -0600

    feat: Implement RabbitMQ integration with ping queue setup and message handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"context"
 	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	// "github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/session"
+	mq "github.com/jackkieny/community-insights/broker"
 	"github.com/jackkieny/community-insights/db"
 	"github.com/jackkieny/community-insights/routes"
 	authenticationRoutes "github.com/jackkieny/community-insights/routes/authentication"
+	posts "github.com/jackkieny/community-insights/routes/post"
 	skoolRoutes "github.com/jackkieny/community-insights/routes/skool"
-    posts "github.com/jackkieny/community-insights/routes/post"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -21,6 +23,17 @@ func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.ANSIC})
 
 	client := db.Init()
+	broker := mq.Init()
+
+	// createQueue, err := mq.SetupQueue(broker, "createQueue")
+	// if err != nil {
+	// 	log.Fatal().Msgf("Failed to setup the \"create\" queue: %s", err)
+	// }
+
+	// deleteQueue, err := mq.SetupQueue(broker, "deleteQueue")
+	// if err != nil {
+	// 	log.Fatal().Msgf("Failed to setup the \"delete\" queue: %s", err)
+	// }
 
 	app := fiber.New()
 	store := session.New()
@@ -44,10 +57,19 @@ func main() {
 	skoolRoutes.GetLabelsRoute(app, client, store)
 	skoolRoutes.RefreshCommunitiesRoute(app, client, store)
 
-    posts.CreatePostRoute(app, store, client)
+	posts.CreatePostRoute(app, store, client)
 
 	routes.SessionRoute(app, store)
 	routes.Setup(app)
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Fatal().Msgf("Error disconnecting from MongoDB: %s", err)
+		}
+		if err := broker.Close(); err != nil {
+			log.Fatal().Msgf("Error disconnecting from RabbitMQ: %s", err)
+		}
+	}()
 
 	if err := app.Listen(":5000"); err != nil {
 		log.Fatal().Msgf("Error starting server: %s", err)
